75c54868373c2beb11d2eba4360dcd7b6c457d4d
Kevin Martinez <ceronk003@gmail.com>
2023-03-16 11:27:52 -0600

    style: cleaning code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,24 +7,30 @@ object Main {
 
         var u1Budget = 0
         var u2Budget = 0
-        //Load Phase-1  from text file
+
+        // Load items for both phases from text files
         val itemPhaseFile1 = simulation.loadItems("spec_files/phase-1.txt")
-        //Load Phase-2 from text file
         val itemPhaseFile2 = simulation.loadItems("spec_files/phase-2.txt")
+
         //Load items into design.U1 Rockets for both phases
         val u1RocketsPhaseOne = simulation.loadU1(ArrayList(itemPhaseFile1.plus(itemPhaseFile2)))
         val u1RocketsPhaseTwo = simulation.loadU1(itemPhaseFile2)
+
         //Run simulations and track budget required for design.U1 rocket
         u1Budget += simulation.runSimulation(u1RocketsPhaseOne)
         u1Budget += simulation.runSimulation(u1RocketsPhaseTwo)
+
         //Load items into design.U2 Rockets for both phases
         val u2RocketsPhaseOne = simulation.loadU2(itemPhaseFile1)
         val u2RocketsPhaseTwo = simulation.loadU2(itemPhaseFile2)
+
         //Run simulations and track budget required for design.U1 rocket
         u2Budget += simulation.runSimulation(u2RocketsPhaseOne)
         u2Budget += simulation.runSimulation(u2RocketsPhaseTwo)
+
         println("Budget required for sending design.U1 Rockets to Mars: $$u1Budget million.")
         println("Budget required for sending design.U2 Rockets to Mars: $$u2Budget million.")
+
         //Display which rocket type to use
         if (u1Budget < u2Budget) {
 
@@ -52,4 +58,5 @@ object Main {
         } else {
             println("Both cost the same. It is okay to use either.")
         }
-    } }
+    }
+}
